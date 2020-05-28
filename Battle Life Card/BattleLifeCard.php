ccb3a49cf3ef4a39f2873398c5945b9150ff8da1
Diego Andrés <diegoandres_cortes@outlook.com>
2020-05-28 17:06:52 -0500

    Initial Commit
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Battle Life Card/BattleLifeCard.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @package ST Shop Battle Life Card
+ * @version 2.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\Modules;
+
+use Shop\Shop;
+use Shop\Helper\Module;
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+class BattleLifeCard extends Module
+{
+	/**
+	 * BattleLifeCard::__construct()
+	 *
+	 * Set the details and basics of the module, along with default values if needed.
+	 */
+	function __construct()
+	{
+		// We will of course override stuff...
+		parent::__construct();
+
+		// Item details
+		$this->authorName = 'Diego Andrés';
+		$this->authorWeb = 'https://smftricks.com/';
+		$this->authorEmail ='admin@smftricks.com';
+		$this->name = Shop::getText('blc_name');
+		$this->desc = Shop::getText('blc_desc');
+		$this->price = 350;
+		$this->require_input = false;
+		$this->can_use_item = true;
+	}
+
+	function onUse()
+	{
+		global $user_info;
+
+		// Bring user back from the dead
+		updateMemberData($user_info['id'], ['is_dead' => 0, 'hp' => 100]);
+		
+		// Display message box
+		return '
+			<div class="infobox">
+				' . Shop::getText('blc_success') . '
+			</div>';
+	}
+}
---
+++ Battle Life Card/README.md
@@ -0,0 +1,20 @@
+ <p align="center">
+    <img src="https://smftricks.com/logos/logo.png">
+ </p>
+ 
+ <img src="https://img.shields.io/badge/License-MPL%202.0-a05a3f?style=flat-square">  <img src="https://img.shields.io/badge/SMF-2.1-3f73a0?style=flat-square">
+
+ 
+## Battle Life Card
+- Developed by [Diego Andrés](https://github.com/DiegoAndresCortes)
+
+## Introduction
+* This module will allow your users to revive in the Battle MOD.
+
+## Requirements
+* [SMF 2.1](https://github.com/SimpleMachines/SMF2.1)
+* [ST Shop](https://github.com/SMFTricks/ST-Shop)
+* [SA Battle](https://github.com/Underdog-01/Battle)
+
+## How to install
+* Simply upload the module through the module section in the ST Shop Admin section.
